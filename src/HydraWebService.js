89697a6b8d61d0a4fbee840cdfb0fbe8ec5794cf
Jeremy Fee <jmfee@usgs.gov>
2016-07-19 10:59:12 -0600

    Add dynamic handler for /event.json

--- src/HydraWebService.js
+++ src/HydraWebService.js
@@ -6,7 +6,10 @@ var express = require('express'),
     extend = require('extend');
 
 
-var _DEFAULTS = {};
+var _DEFAULTS = {
+  MOUNT_PATH: '/ws/hydra',
+  PORT: 8000
+};
 
 
 var HydraWebService = function (options) {
@@ -20,21 +23,45 @@ var HydraWebService = function (options) {
   _this = {};
 
   _initialize = function (options) {
-    options = extend({}, _DEFAULTS, options);
+    options = extend(true, {}, _DEFAULTS, options);
 
     _mountPath = options.MOUNT_PATH;
     _port = options.PORT;
   };
 
 
-  _this.get = function (request, response) {
+  /**
+   * Handle a dynamic get request.
+   *
+   * @param request {HTTPRequest}
+   *     the http request.
+   * @param response {HTTPResponse}
+   *     the http response.
+   * @param next {Middleware}
+   *     next handler in the chain.
+   */
+  _this.get = function (request, response, next) {
+    var url;
 
+    url = request.url.replace(_mountPath, '');
+    if (url === '/event.json') {
+      response.send('Hello from event.json');
+      return;
+    }
+
+    next();
   };
 
+  /**
+   * Run hydra web service in an express server.
+   */
   _this.start = function () {
     var app;
 
     app = express();
+
+    app.get(_mountPath + '/:method', _this.get);
+
     app.use(_mountPath, express.static(__dirname + '/htdocs'));
 
     // TODO: implement server process
@@ -45,6 +72,7 @@ var HydraWebService = function (options) {
       console.log('http://localhost:' + _port + _mountPath + '/');
     });
   };
+
 
   _initialize(options);
   options = null;
