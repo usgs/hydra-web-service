1a295c3fd11d3f6e5cdac46709bd068e206a5135
Jeremy Fee <jmfee@usgs.gov>
2016-07-19 10:29:57 -0600

    Update ExampleTest for coding standards

--- test/spec/ExampleTest.js
+++ test/spec/ExampleTest.js
@@ -3,13 +3,17 @@
 
 
 var chai = require('chai'),
-    expect = chai.expect,
     sinon = require('sinon');
 
 
-describe('ExampleTest', function () {
-  describe('test framework', function () {
+var expect;
 
+expect = chai.expect;
+
+
+describe('ExampleTest', function () {
+
+  describe('test framework', function () {
     it('includes chai', function () {
       expect(typeof chai).to.equal('object');
     });
@@ -17,6 +21,6 @@ describe('ExampleTest', function () {
     it('includes sinon', function () {
       expect(typeof sinon).to.equal('object');
     });
-
   });
+
 });
